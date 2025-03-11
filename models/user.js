dd0e483edefc5e0d5b7d854444032ede05acf459
aditya90585 <adityarajput81971@gmail.com>
2025-03-11 18:23:56 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/miniproject,{strictQuery:false}")
+mongoose.connect("mongodb://localhost:27017/miniproject")
 
 let userschema = mongoose.Schema({
     name:String,
